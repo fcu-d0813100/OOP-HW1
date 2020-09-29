820b40ae129f73406fd70be52f5293fae1a9543f
user <user@EE07>
2020-09-29 09:45:04 +0800

    1

--- src/d0813002/d08131002.java
+++ src/d0813002/d08131002.java
@@ -3,6 +3,10 @@ package d0813002;
 public class d08131002 {
 
 	public static void main(String[] args) {
+		System.out.println("im hugo");
+		System.out.println("d0813100");
+		System.out.println("this is lab one hw");
+		
 		// TODO Auto-generated method stub
 
 	}
